66cd83019760f6c0082250e62f3f958ecf3d9300
namlh <nam281020011@gmail.com>
2025-11-06 00:56:49 +0700

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRouters from "./routes/message.route.js";
 import path from "path";
+import { connectDB } from './lib/db.js';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRouters);
@@ -23,5 +26,8 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-app.listen(PORT, () => console.log("Server running on port: ", PORT));
+app.listen(PORT, () => {
+    console.log("Server running on port: ", + PORT);
+    connectDB();
+});
 
